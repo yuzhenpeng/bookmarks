078b74d6b8b3638b0b8e78fd2a50fad7d3b32b48
guangchuang yu <guangchuangyu@gamil.com>
2018-07-03 10:32:31 +0800

    update script

--- sample_post.R
+++ sample_post.R
@@ -3,7 +3,7 @@
 x <- readLines("biobabble_index.md")
 y <- x[grep("weixin", x)]
 res <- sample(y, 8)
-res <- res[order(nchar(res))]
+res <- res[order(nchar(res, type="width"))]
 cat("----\n")
 cat("**往期精彩**\n")
 cat(paste(res, '\n', collapse=""))
